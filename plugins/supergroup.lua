6dd55350ac22d8065239bb8b6444851b81c79042
hossein101 <hossein_lotg@yahoo.com>
2016-09-01 01:26:00 +0430

    Update supergroup.lua

--- plugins/supergroup.lua
+++ plugins/supergroup.lua
@@ -39,7 +39,7 @@ local function check_member_super(cb_extra, success, result)
       end
       data[tostring(groups)][tostring(msg.to.id)] = msg.to.id
       save_data(_config.moderation.data, data)
-	  local text = 'سوپر گروه اضافه شد!\nاطلاعات شما: [@'..msg.from.username..'] ('..msg.from.id..')
+	  local text = 'سوپر گروه اضافه شد!\nاطلاعات شما: [@'..msg.from.username..'] ('..msg.from.id..')"
       return reply_msg(msg.id, text, ok_cb, false)
     end
   end
