e433ad125ed752ebdeeca6862a607e91e97c082b
hossein101 <hossein_lotg@yahoo.com>
2016-09-01 09:52:19 +0430

    Update time.lua

--- plugins/time.lua
+++ plugins/time.lua
@@ -2,10 +2,10 @@ function run(msg, matches)
 local url , res = http.request('http://api.gpmod.ir/time/')
 if res ~= 200 then return "No connection" end
 local jdat = json:decode(url)
-local text = '🕒 ساعت '..jdat.FAtime..' \n📆 امروز '..jdat.FAdate..' میباشد.\n    ----\n🕒 '..jdat.ENtime..'\n📆 '..jdat.ENdate.. '\n@MoonTeam'
+local text = '🕒 ساعت '..jdat.FAtime..' \n📆 امروز '..jdat.FAdate..' میباشد.\n    ----\n🕒 '..jdat.ENtime..'\n📆 '..jdat.ENdate.. '\n@x_R_E_D_B_O_T_x'
 return text
 end
 return {
-  patterns = {"^[/!#]([Tt][iI][Mm][Ee])$"}, 
+  patterns = {"^(زمان)$"}, 
 run = run 
 }
