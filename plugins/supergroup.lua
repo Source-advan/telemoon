611a7eb50447e053694e356bf582d8098778e603
hossein101 <hossein_lotg@yahoo.com>
2016-09-11 18:03:26 +0430

    Update supergroup.lua

--- plugins/supergroup.lua
+++ plugins/supergroup.lua
@@ -45,7 +45,7 @@ local function check_member_super(cb_extra, success, result)
       end
       data[tostring(groups)][tostring(msg.to.id)] = msg.to.id
       save_data(_config.moderation.data, data)
-	  local text = 'SuperGroup has been added by ['..msg.from.id..'](@'..msg.from.username..')'
+	  local text = '<b>SuperGroup has been added by</b> [<code>'..msg.from.id..'</code>](@'..msg.from.username..')'
       return reply_msg(msg.id, text, ok_cb, false)
     end
   end
@@ -69,7 +69,7 @@ local function check_member_superrem(cb_extra, success, result)
       end
       data[tostring(groups)][tostring(msg.to.id)] = nil
       save_data(_config.moderation.data, data)
-	  local text = 'SuperGroup has been removed by ['..msg.from.id..'](@'..msg.from.username..')'
+	  local text = '<b>SuperGroup has been removed by</b> [<code>'..msg.from.id..'</code>](@'..msg.from.username..')'
       return reply_msg(msg.id, text, ok_cb, false)
     end
   end
