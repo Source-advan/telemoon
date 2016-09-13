fed2f0780e9e2f828d924c98a6b474761d8db657
hossein101 <hossein_lotg@yahoo.com>
2016-09-13 15:58:57 +0430

    Update info-me2.lua

--- plugins/info-me2.lua
+++ plugins/info-me2.lua
@@ -3,13 +3,13 @@ do
 function run(msg, matches)
 local reply_id = msg['id']
 
-local info = '<i>نام :</i> '..msg.from.first_name..'</code>\n'
-..'<i>فامیل :</i> '..(msg.from.last_name or 'ندارد.')..'</code>\n'
-..'<i>آیدی :</i> <code>'..msg.from.id..'</code>\n'
-..'<i>شماره تلفن :</i> <code>+'..(msg.from.phone or 'نامشخص')..'</code>\n'
+local info = '<i>نام : '..msg.from.first_name..'</i>\n'
+..'<i>فامیل : '..(msg.from.last_name or 'ندارد.')..'</i>\n'
+..'<i>آیدی : '..msg.from.id..'</i>\n'
+..'<i>شماره تلفن : +'..(msg.from.phone or 'نامشخص')..'</i>\n'
 ..'<i>نام کاربری :</i> @'..(msg.from.username or 'ندارد')..'\n'
-..'<i>آیدی گروه :</i> <code>'..msg.to.id..'</code>\n'
-..'<i>نام گروه :</i> <code>'..msg.to.title..'</code>\n🔱 @Red_Ch 🔱'
+..'<i>آیدی گروه : '..msg.to.id..'</i>\n'
+..'<i>نام گروه : '..msg.to.title..'</i>\n🔱 @Red_Ch 🔱'
 
 reply_msg(reply_id, info, ok_cb, false)
 end
